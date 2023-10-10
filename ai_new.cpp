9addec018ce53ee0638b1a5aa751abbb1a9e46b5
Magnificientmouse <122452113+Magnificientmouse@users.noreply.github.com>
2023-10-10 20:06:16 +0530

    Update ai_new.cpp

--- ai_new.cpp
+++ ai_new.cpp
@@ -14,15 +14,15 @@ int main(int argc, char *argv[]){
     std::ifstream inputFile(fileName);
 
     inputFile >> n >> m >> k1 >> k2;
-    vector<vector<int>> adj(n, vector<int>(n, 0));
+    vector<vector<int> > adj(n, vector<int>(n, 0));
     vector<int> degree(n, 0);
     for (int i = 0; i < m; i++) {
         int u, v;
         inputFile >> u >> v;
-        adj[u][v] = 1;
-        adj[v][u] = 1;
-        degree[u]++;
-        degree[v]++;
+        adj[u-1][v-1] = 1;
+        adj[v-1][u-1] = 1;
+        degree[u-1]++;
+        degree[v-1]++;
     }
     int num_variables = n+n*k1+n+n*k2;
     vector<string> clauses=vector<string>();
@@ -32,10 +32,10 @@ int main(int argc, char *argv[]){
     clauses.push_back(to_string(n+n*k1+1)+" "+to_string(n+n*k1+n+1)+" 0");
     for(i=0;i<n;i++){
         clauses.push_back(to_string(i+1)+" "+to_string(i+1+n+n*k1)+" 0");
-        if(degree[i]<k1){
+        if(degree[i]<k1-1){
             clauses.push_back(to_string((i+1))+" 0");
         }
-        if(degree[i]<k2){
+        if(degree[i]<k2-1){
             clauses.push_back(to_string((i+1+n+n*k1))+" 0");
         }
         for(j=i+1;j<n;j++){
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]){
             if(j>i){
                 clauses.push_back(to_string(-1*(n+j+1+i*k1))+" 0");
             }
-            else if(i!=0 and j!=0){
+            else if(i!=0 or j!=0){
                 if(j>0){
                     clauses.push_back(to_string(-1*(n+j+1+i*k1))+" "+to_string(n+1+j+(i-1)*k1)+" "+to_string(n+j+(i-1)*k1)+" 0");
                 }
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]){
             if(j>i){
                 clauses.push_back(to_string(-1*(n+j+1+i*k2+n+n*k1))+" 0");
             }
-            else if(i!=0 and j!=0){
+            else if(i!=0 or j!=0){
                 if(j>0){
                     clauses.push_back(to_string(-1*(n+j+1+i*k2+n+n*k1))+" "+to_string(n+1+j+(i-1)*k2+n+n*k1)+" "+to_string(n+j+(i-1)*k2+n+n*k1)+" 0");
                 }
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]){
 
     //Write to file
     std::ofstream outputFile("test.satinput");
-    outputFile << "p cnf "+to_string(n*(k1+k2))+" "+to_string(clauses.size()) << std::endl;
+    outputFile << "p cnf "+to_string(n*(2+k1+k2))+" "+to_string(clauses.size()) << std::endl;
     for (const std::string& clause : clauses) {
         outputFile << clause << std::endl;
     }
